fe5ab557d0a9d30246cc7f5791e0676a82015eef
Alain Makoso (Leny) <106040804+lenynzita@users.noreply.github.com>
2022-11-11 20:39:30 -0500

    fullstack3

---
+++ public/main.js
@@ -0,0 +1,80 @@
+var edit = document.getElementsByClassName("fa-edit");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
+var trash = document.getElementsByClassName("fa-trash-o");
+
+Array.from(edit).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const tips= parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        const newPrice = prompt("what is your new price?")
+        const newTips = prompt("what is your new tips?(0,5,10,20,50)")
+        let tipOption = [0,5,10,20,50]
+        if (tipOption.includes(parseInt(newTips)) && parseInt(newPrice) ){
+
+      
+        fetch('messages', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'tips':tips,
+            'newPrice':newPrice,
+            'newTips':newTips
+            
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      }
+      });
+});
+
+Array.from(thumbDown).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+    fetch('animals', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg,
+        'thumbUp':thumbUp
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
